187616d79bfb05de943ebb5591c9ec795b1506f2
BSpitz00 <brandonjspitz@gmail.com>
2023-01-24 21:10:14 -0700

    Added a scanner for input and expanded options

--- main.go
+++ main.go
@@ -1,72 +1,89 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
 func main() {
 	fuel := 1000000
-	fmt.Println("What planet would you like to visit?")
-	fmt.Println("Choices: Venus, Mercury, Mars")
 
-	planetChoice := "" //Enter choice here
+	for fuel > 0 {
+		fmt.Println("\nWhat planet in our solar system would you like to visit?")
 
-	fuel = flyToPlanet(planetChoice, fuel)
-	fuelGauge(fuel)
-	cost(fuel)
-	timothySmells()
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		planetChoice := scanner.Text()
 
+		fuel = flyToPlanet(planetChoice, fuel)
+		fuelGauge(fuel)
+		cost(fuel)
+		youSmell()
+	}
 }
 
 func fuelGauge(fuel int) {
-	fmt.Println("You have", fuel, "gallons of fuel left!")
+	fmt.Println("you have", fuel, "gallons of fuel left!")
 }
 
 func calculateFuel(planet string) int {
 	switch planet {
-	case "Venus":
-		return 300000
+	case "The Sun":
+		return 93000
 	case "Mercury":
-		return 500000
+		return 58000
+	case "Venus":
+		return 26000
+	case "The Moon":
+		return 1000
 	case "Mars":
-		return 700000
+		return 49000
+	case "Jupiter":
+		return 391000
+	case "Saturn":
+		return 796000
+	case "Uranus":
+		return 1697000
+	case "Neptune":
+		return 2787000
+	case "Pluto":
+		return 3577000
 	default:
 		return 0
 	}
 }
 
 func greetPlanet(planet string) {
-	fmt.Printf("Welcome to planet %v", planet)
+	fmt.Printf("Welcome to planet %v, ", planet)
 }
 
 func cantFly() {
-	fmt.Print("We do not have the available fuel to fly there.")
+	fmt.Print("Too far! We do not have the available fuel to fly there, you're stuck!")
 }
 
 func flyToPlanet(planet string, fuel int) int {
 	fuelRemaining := fuel
 	fuelCost := calculateFuel(planet)
 
-	if fuelRemaining < fuelCost {
+	if fuelRemaining >= fuelCost {
+		greetPlanet(planet)
+		fuelRemaining -= fuelCost
+	} else {
 		cantFly()
-		return -1
 	}
-
-	greetPlanet(planet)
-	fuelRemaining -= fuelCost
-
-	return fuelRemaining - fuelCost
+	return fuelRemaining
 }
 
 func cost(fuel int) {
 	cost := fuel * 4
-	fmt.Printf("The cost of fuel for this trip is $%v... Thanks Mr. President", cost)
+	fmt.Printf("The cost of fuel for this trip is $%v.", cost)
 }
 
-func timothySmells() {
+func youSmell() {
 	rand.Seed(time.Now().UnixNano())
 	aliens := rand.Intn(1000)
-	fmt.Printf("%v inhabitants on planet think Timothy smells", aliens)
+	fmt.Printf(" %v Inhabitants on planet think you smell funny.", aliens)
 }
